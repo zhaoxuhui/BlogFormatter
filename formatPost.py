59cdd616a487688ae629894c1ca91a75820e20d9
zhaoxuhui <zhaoxuhui1993@qq.com>
2018-05-01 23:56:09 +0800

    push

---
+++ formatPost.py
@@ -0,0 +1,14 @@
+import toc
+import BlogImages
+
+flag0 = raw_input("Auto generate IMG?y/n\n")
+if flag0 == "y":
+    print("Format images...\n")
+    out_res = BlogImages.execImgs()
+
+flag1 = raw_input("\nInsert TOC?y/n\n")
+if flag1 == "y":
+    print("\nInsert TOC...\n")
+    toc.execFunction(out_res)
+else:
+    exit()
